032c527e820a0f302535e0205d152f2613a0388e
Jaisoft <jaime.gomez.moraleda@gmail.com>
2020-10-08 00:35:23 +0200

    first commit

---
+++ src/main/java/com/jaisoft/Models/Car.java
@@ -0,0 +1,5 @@
+package com.jaisoft.Models;
+
+public class Car {
+    
+}
---
+++ src/main/java/com/jaisoft/Models/User.java
@@ -0,0 +1,16 @@
+package com.jaisoft.Models;
+
+public class User {
+
+    private String name;
+    private Integer edad;
+    private Car car;
+
+    public User (String name, Integer edad, Car car){
+        this.edad = edad;
+        this.name = name; 
+        this.car=car;
+    }
+    
+    
+}
